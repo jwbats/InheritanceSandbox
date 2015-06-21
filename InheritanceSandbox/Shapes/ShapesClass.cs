298b32eebad9208cc4c83358785d1f456e9ff902
jwbats <jwbats@gmail.com>
2015-06-21 18:39:42 +0200

    Initial commit.

---
+++ InheritanceSandbox/Shapes/ShapesClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSandbox.Shapes
+{
+	abstract class ShapesClass
+	{
+		// abstract: no body, must be implemented by deriving class
+		abstract public int Area1();
+
+		// virtual: body required, may be overridden (override) or hidden (new) by deriving class
+		virtual public int Area2(int a, int b)
+		{
+			return a * b;
+		}
+
+		// virtual: body required, may be overridden (override) or hidden (new) by deriving class
+		virtual public int Area3(int a, int b)
+		{
+			return a * b;
+		}
+
+		// virtual: body required, may be overridden (override) or hidden (new) by deriving class
+		virtual public int Area4(int a, int b)
+		{
+			return a * b;
+		}
+
+	}
+}
---
+++ InheritanceSandbox/Shapes/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSandbox.Shapes
+{
+
+	class Square : ShapesClass
+	{
+		private int side = 0;
+
+		public Square(int n)
+		{
+			side = n;
+		}
+
+		// abstract area method must be implemented using the override keyword
+		public override int Area1()
+		{
+			return side * side;
+		}
+
+		// virtual area method may be implemented using override keyword
+		public override int Area2(int a, int b)
+		{
+			return base.Area2(a, b);
+		}
+
+		// virtual area method may be implemented using override keyword
+		public new int Area3(int a, int b)
+		{
+			return base.Area3(a, b);
+		}
+		
+	}
+
+}
